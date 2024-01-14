6222f38740e4fa58aa3bf22e80930de925a34b6a
ilker <ilker897@gmail.com>
2024-01-14 22:44:15 +0300

    init

---
+++ src/pages/constants.ts
@@ -0,0 +1,34 @@
+export const portfolioConstants = {
+  name: "İlker Sarıca",
+  about: [
+    "I'm a React Native developer with a passion for creating mobile applications that are not only functional but also visually stunning. Over time, I've become proficient in different technologies such as React Native, JavaScript, and Node.js. As a developer, I love to collaborate with designers, product managers, and other developers to deliver high-quality applications that solve real-world problems.",
+  ],
+  skills: [
+    {
+      name: "Typescript",
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg",
+    },
+    {
+      name: "React",
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    },
+    {
+      name: "React Native",
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    },
+    {
+      name: "Node",
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    },
+    {
+      name: "MongoDB",
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+    },
+  ],
+  interests: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+};
