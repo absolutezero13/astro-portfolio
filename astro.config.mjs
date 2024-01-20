db53851f24bc5517e2104e5a31e8619c61ba7f8b
ilker <ilker897@gmail.com>
2024-01-20 18:20:16 +0300

    fix: ready for netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://absolutezero13.github.io",
-  base: "/astro-portfolio",
-});
+export default defineConfig({});
